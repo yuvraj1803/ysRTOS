1ebedd657c54e3c1793df43541d02ae03a42bd48
Yuvraj Sakshith <ysakshith@gmail.com>
2023-02-02 17:48:46 +0530

    added bit flags in ADC.c

--- STM32F4/F411RETx/Src/drivers/ADC/ADC.c
+++ STM32F4/F411RETx/Src/drivers/ADC/ADC.c
@@ -9,27 +9,42 @@
  ******************************************************************************
  */
 
+#define GPIOAEN		(1U << 0)
+
+#define ADC1EN		(1U << 8);
+#define ADC1ON		(1U << 0);
+#define ADC1OFF			0
+#define ADC1SWSTART	(1U << 30)
+#define ADC1EOC		(1U << 1)
 
 void ADC1_INIT(void){
 
-	RCC->AHB1ENR |= 1; /* enable clock for GPIOA */
-	GPIOA->MODER |= 0xC; /* set pin 1 as analog */
+	RCC->AHB1ENR |= GPIOAEN; /* enable clock for GPIOA */
+	GPIOA->MODER |= ((1U << 2) | (1U << 3)); /* set pin 1 as analog, 2y:2y+1 */
 
-	RCC->APB2ENR |= 0x00000100;
-	ADC1->CR2 = 0;
-	ADC1->SQR3 = 1; /* conversion sequence start at channel 1*/
-	ADC1->SQR1 = 0; /* length of sequence is 1 */
-	ADC1->CR2 |= 1;
+	RCC->APB2ENR |= ADC1EN;
+	ADC1->CR2 =  ADC1OFF;  	/* turn off ADC1 */
+	ADC1->SQR3 = 1; 		/* conversion sequence start at channel 1*/
+	ADC1->SQR1 = 0;			/* length of sequence is 1 */
+	ADC1->CR2 |= ADC1ON;	/* turn on ADC1 */
 
 }
 
 uint32_t ADC1_READ(void){
 
-	ADC1->CR2 |= 0x40000000; /* begin conversation */
+	ADC1->CR2 |= ADC1CONV; /* begin conversation */
 
-	while(!(ADC1->SR & 2)){} /* wait for conversation to complete */
+	while(!(ADC1->SR & ADC1EOC)){} /* wait for conversation to complete */
 
 
 	return ADC1->DR;
+
+}
+
+void ENABLE_ADC1_SEQ(uint8_t SQn, uint8_t CHn){ /* Assigns Channel CHn to Sequence SQn */
+
+}
+
+void DISABLE_ADC1_SEQ(uint8_t SQn, uint8_t CHn){
 
 }
